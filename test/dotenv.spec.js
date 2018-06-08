2bf2e9118ee5bcd3dd4a25ff28f434960f18d64f
Alex Serdukov <a.serdukov@mobilunity.com>
2018-06-08 15:38:45 +0300

    example

---
+++ test/dotenv.spec.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai');
+const { config } = require('dotenv');
+
+describe('DotEnv', () => {
+  beforeEach(() => config());
+
+  it('should parse equality chars', () => {
+    expect(process.env.PASSWORD).to.equal('somePa$$With=Equalch@R');
+  });
+});
